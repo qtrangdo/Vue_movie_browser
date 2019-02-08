d01a076b6c0eff7318f6d7b4945dcbe40adb9964
td94 <quynhtrangdo94@gmail.com>
2019-02-07 22:35:06 -0800

    Set up API request

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    <SearchBar></SearchBar>
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
   import SearchBar from './components/SearchBar';
+  import API_KEY from './config.js';
+  const key = API_KEY;
   export default {
     name: "App",
     components: {
       SearchBar
+    },
+    methods: {
+      onTermChange(searchTerm) {
+        axios.get('https://googleapis.com/youtube/v3/search', {
+          params: {
+            key,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm,
+          }
+        })
+          .then( res => console.log(res))
+      }
     }
   };
 </script>
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'AIzaSyDDa_sw3OQORWFEfDSpL1NYrz-e9ltcV9g';
