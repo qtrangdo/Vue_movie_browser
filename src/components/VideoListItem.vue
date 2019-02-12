7ca9c3443832a92470986423f94527eb007620b4
td94 <quynhtrangdo94@gmail.com>
2019-02-11 22:50:03 -0800

    Use v-for directive to pass props down

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,11 +1,14 @@
 <template>
   <li>
-    VideoList Item
+    {{ video.snippet.title }}
   </li>
 </template>
 
 <script>
 export default {
-  name: 'VideoListItem'
+  name: 'VideoListItem',
+  props: {
+    video: Object
+  }
 };
 </script>
