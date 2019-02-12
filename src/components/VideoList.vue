127b29709e008a3ea32b1608c44363d1f3d9634e
td94 <quynhtrangdo94@gmail.com>
2019-02-11 22:22:20 -0800

    Set up VideoListItem component

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,15 +1,20 @@
 <template>
   <ul>
     VideoList
+    {{ videos.length }}
   </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem'
 export default {
   name: 'VideoList',
+  components: {
+    VideoListItem,
+  },
   props: {
     videos: Array,
-  }
+  },
 }
 </script>
 
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>
+    VideoList Item
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'VideoListItem'
+};
+</script>
