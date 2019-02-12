9bf466f99c9d0db5a8d1c7b5175ea0e2bf24e9f5
td94 <quynhtrangdo94@gmail.com>
2019-02-11 23:37:43 -0800

    Add styling using bootstrap

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul>
+  <ul class="list-group">
     <VideoListItem 
       v-for="video in videos"
       :video="video"
@@ -23,5 +23,5 @@ export default {
 </script>
 
 <style>
-
+  
 </style>
--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,5 +1,6 @@
 <template>
-  <li>
+  <li class="list-group-item">
+    
     {{ video.snippet.title }}
   </li>
 </template>
