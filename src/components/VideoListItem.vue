b5469fa28cd04e416d37975a4f4b465d8ad7073c
td94 <quynhtrangdo94@gmail.com>
2019-02-11 23:50:17 -0800

    Add thumbnails and more style

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,15 +1,32 @@
 <template>
-  <li class="list-group-item">
-    
-    {{ video.snippet.title }}
+  <li class="list-group-item media">
+    <img class="mr-3" :src="thumbnailUrl">
+    <div class="media-body">{{ video.snippet.title }}</div>
   </li>
 </template>
 
 <script>
 export default {
-  name: 'VideoListItem',
+  name: "VideoListItem",
   props: {
     video: Object
+  },
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    }
   }
 };
 </script>
+
+<style scoped>
+  li {
+    display: flex;
+    cursor: pointer;
+  }
+
+  li:hover {
+    background-color: #eee 
+  }
+</style>
+
